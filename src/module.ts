766e33db5f81bdb0dabd3d62af9055163d718879
Norman F <nf@pocket-rocket.io>
2024-05-12 21:27:42 +0200

    completely redid tailwind stuff

--- src/module.ts
+++ src/module.ts
@@ -4,32 +4,30 @@ import {
   defineNuxtModule,
   installModule,
 } from "@nuxt/kit";
-import installTailwind from "./tailwind";
 
-// Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface ModuleOptions { }
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
     name: "vue3-better-pick",
     configKey: "Vue3BetterPick",
   },
-  // Default configuration options of the Nuxt module
   defaults: {},
   async setup(_options, nuxt) {
     const resolver = createResolver(import.meta.url);
 
+    nuxt.options.css.push(resolver.resolve("./tailwind.min.css"));
+
     await installModule("nuxt-icon");
-    await installTailwind(nuxt, resolver.resolve);
-
-    // nuxt.options.modules.push("@nuxtjs/tailwindcss", "nuxt-icon");
-
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
+    await installModule("@nuxtjs/tailwindcss", {
+      configPath: resolver.resolve("./runtime/tailwind.config.js"),
+    }
+    );
     await addComponentsDir({
-      path: resolver.resolve("./runtime/components"), // path of components
-      pathPrefix: false, // Prefix component name by its path.
-      prefix: "", // Prefix all matched components.
-      global: true, // Registers components to be globally available.
+      path: resolver.resolve("./runtime/components"),
+      pathPrefix: false,
+      prefix: "",
+      global: true,
     });
   },
 });
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    prefix: "",
+    content: ["./src/runtime/**/*.{js,ts,vue,css}"],
+    theme: {},
+    plugins: [],
+};
